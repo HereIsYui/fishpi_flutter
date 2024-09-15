933035e543f5079611d957443572261d61710df8
Yui <529247970@qq.com>
2024-09-15 15:34:30 +0800

    add IM event

--- lib/core/controller/im.dart
+++ lib/core/controller/im.dart
@@ -19,4 +19,56 @@ class IMController extends GetxController {
       return Future.error(e!);
     });
   }
+
+  Future<void> init(String token) async {
+    fishpi = Fishpi(token);
+  }
+
+  Future<List<ChatData>> getChatList() async {
+    List<ChatData> list = await fishpi.chat.list();
+    return list;
+  }
+
+  Future<void> chatInit() async {
+    fishpi.chatroom.addListener((ChatRoomData data) {
+      try {
+        switch (data.type) {
+          case ChatRoomMessageType.online:
+            // 上线消息
+            print(data.online!);
+            break;
+          case ChatRoomMessageType.barrager:
+            // 弹幕消息
+            print(data.barrager!);
+            break;
+          case ChatRoomMessageType.discussChanged:
+            // 话题变更
+            print(data.discuss!);
+            break;
+          case ChatRoomMessageType.msg:
+            // 普通消息
+            print(data.msg!.content);
+            break;
+          case ChatRoomMessageType.revoke:
+            // 撤回消息
+            print(data.revoke!);
+            break;
+          case ChatRoomMessageType.redPacket:
+            // 红包消息
+            print(data.msg!);
+            break;
+          case ChatRoomMessageType.redPacketStatus:
+            // 红包领取
+            print(data.status!);
+            break;
+          case ChatRoomMessageType.custom:
+            // 自定义消息
+            print(data.custom!);
+            break;
+        }
+      } catch (e) {
+        print('未知异常：$e');
+      }
+    });
+  }
 }
