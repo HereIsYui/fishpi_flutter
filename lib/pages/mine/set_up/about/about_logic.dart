eca5846894cb5f0629ce9c92b158404526d720ac
Yui <529247970@qq.com>
2024-09-20 00:22:21 +0800

    添加关于我们页面

--- lib/pages/mine/set_up/about/about_logic.dart
+++ lib/pages/mine/set_up/about/about_logic.dart
@@ -1,10 +1,20 @@
 import 'package:get/get.dart';
-
+import 'package:package_info_plus/package_info_plus.dart';
 
 class AboutLogic extends GetxController {
+  final version = '3.0.0'.obs;
   @override
   void onInit(){
-    
+    initInfo();
     super.onInit();
+  }
+
+  void initInfo() async{
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    String appName = packageInfo.appName;
+    String packageName = packageInfo.packageName;
+    version.value = packageInfo.version;
+    String buildNumber = packageInfo.buildNumber;
+    print('appName:$appName,packageName:$packageName,version:${version.value},buildNumber:$buildNumber');
   }
 }
--- lib/pages/mine/set_up/about/about_view.dart
+++ lib/pages/mine/set_up/about/about_view.dart
@@ -1,4 +1,8 @@
+import 'package:fishpi_app/res/styles.dart';
+import 'package:fishpi_app/widgets/pi_image.dart';
+import 'package:fishpi_app/widgets/pi_title_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import 'about_logic.dart';
@@ -6,10 +10,94 @@ import 'about_logic.dart';
 class AboutPage extends StatelessWidget {
   final AboutLogic logic = Get.put(AboutLogic());
 
-  AboutPage({Key? key}) : super(key: key);
+  AboutPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Obx(
+      () => Scaffold(
+        backgroundColor: Styles.primaryColor,
+        body: SizedBox(
+          width: 1.sw,
+          height: 1.sh,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).padding.top,
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Get.back();
+                },
+                behavior: HitTestBehavior.translucent,
+                child: Container(
+                  width: 1.sw - 32.w,
+                  height: 40.w,
+                  alignment: Alignment.centerLeft,
+                  child: Icon(Icons.arrow_back_ios_outlined),
+                ),
+              ),
+              100.verticalSpace,
+              Image.asset(
+                'assets/images/logo.png',
+                width: 70.w,
+                height: 70.w,
+              ),
+              20.verticalSpace,
+              Text(
+                '摸鱼派',
+                style: TextStyle(
+                  fontSize: 20.sp,
+                  color: Styles.primaryTextColor,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Version ${logic.version.value}',
+                style: TextStyle(
+                  fontSize: 16.sp,
+                  color: Styles.primaryTextColor,
+                ),
+              ),
+              Expanded(
+                child: 1.verticalSpace,
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Text(
+                  '《用户隐私协议》',
+                  style: TextStyle(
+                    fontSize: 12.sp,
+                    color: Colors.blueGrey,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              10.verticalSpace,
+              Text(
+                '北京白与画科技有限公司 版权所有',
+                style: TextStyle(
+                  fontSize: 10.sp,
+                  color: Styles.primaryTextColor,
+                ),
+              ),
+              2.verticalSpace,
+              Text(
+                'Copyright © 2021 - 2024 W&P Tech. All Rights Reserved.',
+                style: TextStyle(
+                  fontSize: 10.sp,
+                  color: Styles.primaryTextColor,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              50.verticalSpace,
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
