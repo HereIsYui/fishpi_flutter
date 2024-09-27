93fd63dd3d3d4ceec0e10b60ef2c22becbdafbd9
Yui <44658840+HereIsYui@users.noreply.github.com>
2024-09-27 17:23:13 +0800

    添加文章评论

--- lib/pages/forum/detail/forum_detail_view.dart
+++ lib/pages/forum/detail/forum_detail_view.dart
@@ -1,5 +1,8 @@
+import 'package:fishpi/types/article.dart';
 import 'package:fishpi_app/res/styles.dart';
+import 'package:fishpi_app/utils/pi_utils.dart';
 import 'package:fishpi_app/widgets/loading.dart';
+import 'package:fishpi_app/widgets/pi_avatar.dart';
 import 'package:fishpi_app/widgets/pi_title_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_emoji/flutter_emoji.dart';
@@ -48,110 +51,145 @@ class ForumDetailPage extends StatelessWidget {
                     ),
                     const Divider(),
                     ...buildMarkdown(),
-                    Container(
-                      width: 1.sw - 32.w,
-                      child: Stack(
-                        children: [
-                          Positioned(
-                            child: PiZebraStripesBack(
-                              width: 1.sw - 32.w,
-                              height: 88.h,
-                              lineWidth: 10.w,
-                              lineColor: Styles.c4Color,
-                              spacing: 10.w,
+                    if (logic.article.value.rewardPoint != 0)
+                      SizedBox(
+                        width: 1.sw - 32.w,
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              child: PiZebraStripesBack(
+                                width: 1.sw - 32.w,
+                                height: 88.h,
+                                lineWidth: 10.w,
+                                lineColor: Styles.c4Color,
+                                spacing: 10.w,
+                              ),
                             ),
-                          ),
-                          logic.article.value.rewarded
-                              ? Positioned(
-                                  child: GestureDetector(
-                                    onTap: () {
-                                      // 打赏
-                                    },
-                                    child: Container(
-                                      width: 1.sw - 32.w,
-                                      padding: EdgeInsets.all(10.w),
-                                      child: Column(
-                                        mainAxisSize: MainAxisSize.max,
-                                        children: [
-                                          Text(
-                                            logic.article.value.rewardContent,
-                                            style: TextStyle(
-                                              color: Styles.primaryTextColor,
-                                              fontSize: 16.sp,
-                                            ),
-                                            textAlign: TextAlign.left,
-                                          ),
-                                          Text(
-                                            '${logic.article.value.rewardedCnt}人打赏',
-                                            style: TextStyle(
-                                              color: Styles.primaryTextColor,
-                                              fontSize: 16.sp,
-                                            ),
-                                            textAlign: TextAlign.right,
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                )
-                              : Positioned(
-                                  child: GestureDetector(
-                                    onTap: () {
-                                      // 打赏
-                                    },
-                                    child: Container(
-                                      width: 1.sw - 32.w,
-                                      height: 88.h,
-                                      padding: EdgeInsets.all(10.w),
-                                      child: Column(
-                                        mainAxisSize: MainAxisSize.max,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          SizedBox(
-                                            width: 1.sw - 32.w,
-                                            child: RichText(
-                                              text: TextSpan(
-                                                text: '打赏',
-                                                style: TextStyle(
-                                                  color:
-                                                      const Color(0xFFED8F26),
-                                                  fontSize: 16.sp,
-                                                  fontWeight: FontWeight.bold,
-                                                ),
-                                                children: [
-                                                  TextSpan(
-                                                    text:
-                                                        '${logic.article.value.rewardPoint}积分后可见',
-                                                    style: TextStyle(
-                                                      fontSize: 16.sp,
-                                                      color: Styles
-                                                          .primaryTextColor,
-                                                    ),
-                                                  ),
-                                                ],
+                            logic.article.value.rewarded
+                                ? Positioned(
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        // 打赏
+                                      },
+                                      child: Container(
+                                        width: 1.sw - 32.w,
+                                        padding: EdgeInsets.all(10.w),
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.max,
+                                          children: [
+                                            Text(
+                                              logic.article.value.rewardContent,
+                                              style: TextStyle(
+                                                color: Styles.primaryTextColor,
+                                                fontSize: 16.sp,
                                               ),
+                                              textAlign: TextAlign.left,
                                             ),
-                                          ),
-                                          SizedBox(
-                                            width: 1.sw - 32.w,
-                                            child: Text(
+                                            Text(
                                               '${logic.article.value.rewardedCnt}人打赏',
                                               style: TextStyle(
-                                                color: const Color(0xFFED8F26),
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 12.sp,
+                                                color: Styles.primaryTextColor,
+                                                fontSize: 16.sp,
                                               ),
                                               textAlign: TextAlign.right,
                                             ),
-                                          ),
-                                        ],
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                : Positioned(
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        // 打赏
+                                      },
+                                      child: Container(
+                                        width: 1.sw - 32.w,
+                                        height: 88.h,
+                                        padding: EdgeInsets.all(10.w),
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.max,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            SizedBox(
+                                              width: 1.sw - 32.w,
+                                              child: RichText(
+                                                text: TextSpan(
+                                                  text: '打赏',
+                                                  style: TextStyle(
+                                                    color:
+                                                        const Color(0xFFED8F26),
+                                                    fontSize: 16.sp,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                  children: [
+                                                    TextSpan(
+                                                      text:
+                                                          '${logic.article.value.rewardPoint}积分后可见',
+                                                      style: TextStyle(
+                                                        fontSize: 16.sp,
+                                                        color: Styles
+                                                            .primaryTextColor,
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                            SizedBox(
+                                              width: 1.sw - 32.w,
+                                              child: Text(
+                                                '${logic.article.value.rewardedCnt}人打赏',
+                                                style: TextStyle(
+                                                  color:
+                                                      const Color(0xFFED8F26),
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 12.sp,
+                                                ),
+                                                textAlign: TextAlign.right,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
-                        ],
+                          ],
+                        ),
                       ),
+                    const Divider(),
+                    10.verticalSpace,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          '评论 ${logic.article.value.commentCnt}',
+                          style: TextStyle(
+                            color: Styles.primaryTextColor,
+                            fontSize: 14.sp,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: (){},
+                          child: Text(
+                            '写评论',
+                            style: TextStyle(
+                              color: Styles.primaryColor,
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    10.verticalSpace,
+                    ListView.builder(
+                      padding: EdgeInsets.zero,
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemBuilder: buildCommentItem,
+                      itemCount: logic.article.value.comments.length,
                     )
                   ],
                 ),
@@ -176,6 +214,79 @@ class ForumDetailPage extends StatelessWidget {
             ),
             onTap: (url) {},
           )
+        ],
+      ),
+    );
+  }
+
+  Widget buildCommentItem(BuildContext context, int index) {
+    ArticleComment item = logic.article.value.comments[index];
+    return Container(
+      width: 1.sw - 32.w,
+      margin: EdgeInsets.only(bottom: 20.h),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          PiAvatar(
+            userName: item.author,
+            avatarURL: item.thumbnailURL,
+            width: 30.w,
+            height: 30.w,
+          ),
+          10.horizontalSpace,
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          item.author,
+                          style: TextStyle(
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.bold,
+                            color: Styles.primaryTextColor,
+                          ),
+                        ),
+                        10.horizontalSpace,
+                        Text(
+                          item.timeAgo,
+                          style: TextStyle(
+                            fontSize: 12.sp,
+                            color: Styles.c4Color,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset(
+                          'assets/images/unlike.png',
+                          width: 20.w,
+                          height: 20.w,
+                        ),
+                        Text(
+                          '${item.goodCnt}',
+                          style: TextStyle(
+                            fontSize: 12.sp,
+                            color: Styles.c4Color,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                PiUtils.getChatPreview(item.content),
+              ],
+            ),
+          ),
         ],
       ),
     );
